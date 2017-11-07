89b2b23773926d44d27a8f26c87b73be2e9b4b3a
Onorio Catenacci <ocatenacci@unitedshore.com>
2017-11-07 16:44:46 -0500

    Adding a struct for CNG packages
    I started to add a struct to contain the common details for each of the
    ChocolateyNuGet packages that I want to maintain.

--- lib/choco_util.ex
+++ lib/choco_util.ex
@@ -1,4 +1,8 @@
 defmodule ChocoUtil do
+  defmodule Package do
+    defstruct url_template: "", current_version: "", binary_name: ""
+  end
+
   defp get_sha256(file) do
     {result_string,0} = System.cmd("certutil",["-hashfile", "#{file}","SHA256"],[])
     [_,sha256,_,_] = String.split(result_string, "\r\n")
@@ -20,5 +24,13 @@ defmodule ChocoUtil do
     get_file_from_remote(precompiled_zip, remote_url)
     sha256 = get_sha256(precompiled_zip)
     sha256
+  end
+
+  def initialize_packages do
+    elixir_package = %Package{
+      url_template: ~S('https://github.com/elixir-lang/elixir/releases/download/v#{version}/Precompiled.zip'),
+      current_version: "1.5.2",
+      binary_name: "precompiled.zip"
+    }
   end
 end
