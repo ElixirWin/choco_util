c3a63c86ad4151e56abe2a40c904e9c3948f6eb8
Onorio Catenacci <ocatenacci@unitedshore.com>
2017-11-07 13:52:48 -0500

    Wrapping file fetch and sha256 generation
    I've wrapped the file fetch and the sha256 generation inside another
    function.

--- lib/choco_util.ex
+++ lib/choco_util.ex
@@ -1,16 +1,24 @@
 defmodule ChocoUtil do
-  def get_sha256(file) do
+  defp get_sha256(file) do
     {result_string,0} = System.cmd("certutil",["-hashfile", "#{file}","SHA256"],[])
     [_,sha256,_,_] = String.split(result_string, "\r\n")
     sha256
   end
 
-  def get_file_from_remote(filename, url) do
+  defp get_file_from_remote(filename, url) do
     Application.ensure_all_started :inets 
     :ssl.start
     {:ok, resp} = :httpc.request(:get, {url, []}, [], [body_format: :binary])
     {{_, 200, 'OK'}, _headers, body} = resp
 
     File.write!(filename, body) 
+  end
+
+  def get_file_and_sha256(version) do
+    precompiled_zip = System.get_env("temp")<>"\\precompiled.zip"
+    remote_url = 'https://github.com/elixir-lang/elixir/releases/download/v#{version}/Precompiled.zip'
+    get_file_from_remote(precompiled_zip, remote_url)
+    sha256 = get_sha256(precompiled_zip)
+    sha256
   end
 end
