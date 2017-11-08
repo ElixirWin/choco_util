a6def2efc8022a5ffaacaa81f97b1b966ab5a879
Onorio Catenacci <ocatenacci@unitedshore.com>
2017-11-08 09:28:24 -0500

    I added package specs for Erlang x86, Erlang x64 and Rebar
    I began to add package specs for both versions of Erlang and Rebar too.
    I also added some code to work through the package list; this will cause
    it to pull all the right binaries and generate ths SHA256 for each of
    them.

--- lib/choco_util.ex
+++ lib/choco_util.ex
@@ -1,14 +1,33 @@
 defmodule ChocoUtil do
   defmodule Package do
-    defstruct url_template: "", current_version: "", binary_name: ""
+    defstruct url_template: "", current_version: "", download_directory: System.get_env("temp"), binary_name: ""
   end
 
-  def initialize_packages do
+  defp initialize_packages do
     elixir_package = %Package{
-      url_template: ~S('https://github.com/elixir-lang/elixir/releases/download/v#{current_version}/Precompiled.zip'),
+      url_template: 'https://github.com/elixir-lang/elixir/releases/download/v1.5.2/Precompiled.zip',
       current_version: "1.5.2",
-      binary_name: System.get_env("temp") <> "/precompiled.zip"
+      binary_name: "/precompiled.zip"
     }
+    
+    rebar3_package = %Package{
+      url_template: 'https://s3.amazonaws.com/rebar3/rebar3',
+      current_version: "3.4.4",
+      binary_name: "/rebar3"
+    }
+
+    erlang_w32_package = %Package{
+      url_template: 'http://www.erlang.org/downloads/otp_win32_20.1.exe',
+      current_version: "20.1",
+      binary_name: "/opt_win32_20.1.exe"
+    }
+
+    erlang_w64_package = %Package{
+      url_template: 'http://www.erlang.org/downloads/otp_win64_20.1.exe',
+      current_version: "20.1",
+      binary_name: "/opt_win64_20.1.exe"
+    }
+    [elixir_package, rebar3_package, erlang_w32_package, erlang_w64_package]
   end
 
   defp get_sha256(file) do
@@ -17,20 +36,27 @@ defmodule ChocoUtil do
     sha256
   end
 
-  defp get_file_from_remote(filename, url) do
+  defp get_file_from_remote(download_dir, filename, url) do
     Application.ensure_all_started :inets 
     :ssl.start
     {:ok, resp} = :httpc.request(:get, {url, []}, [], [body_format: :binary])
     {{_, 200, 'OK'}, _headers, body} = resp
 
-    File.write!(filename, body) 
+    File.write!(download_dir <> filename, body) 
   end
 
-  def get_file_and_sha256(version) do
-    precompiled_zip = System.get_env("temp")<>"\\precompiled.zip"
-    remote_url = 'https://github.com/elixir-lang/elixir/releases/download/v#{version}/Precompiled.zip'
-    get_file_from_remote(precompiled_zip, remote_url)
-    sha256 = get_sha256(precompiled_zip)
+  def get_file_and_sha256(package_details = %Package{}) do
+    download_directory = package_details.download_directory
+    filename = package_details.binary_name
+    remote_url = package_details.url_template
+    get_file_from_remote(download_directory, filename, remote_url)
+    sha256 = get_sha256(download_directory <> filename)
     sha256
   end
+
+  def get_package_and_sha256() do 
+    for p <- initialize_packages() do 
+      get_file_and_sha256(p)
+    end
+  end    
 end
