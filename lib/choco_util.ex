04806ee1dbc75754b09f164c6ed6888912aca604
Onorio Catenacci <ocatenacci@unitedshore.com>
2017-12-04 16:20:44 -0500

    More work to simplify the code
    Attempting to simplify the code to remove redundancy and attempting to
    add code to filter so that the code doesn't attempt to modify the file
    name of files which don't have the eex extension.

--- lib/choco_util.ex
+++ lib/choco_util.ex
@@ -3,6 +3,8 @@ defmodule ChocoUtil do
   @erlang_x86_package_name "Erlang_x86"
   @erlang_x64_package_name "Erlang_x64"
   @rebar_package_name "Rebar3"
+  @file_separator  "."
+  @eex_extension "eex"
 
   defp initialize_package(@elixir_package_name) do
     elixir_package = %Package{
@@ -62,25 +64,29 @@ defmodule ChocoUtil do
     File.write!(download_dir <> filename, body) 
   end
 
-  defp get_base_file_name_from_template_name(template_file_name) do
-    file_separator = "."
-    [base, extension, _eexExtension] = String.split(template_file_name, file_separator)
-    base <> file_separator <> extension   
+  defp filter_file_list(file_list) do
+    for f <- file_list, [_base, _extension, @eex_extension] <- String.split(f,@file_separator), do: f
   end
 
-  def fix_up(pn = @rebar_package_name) do
+  defp get_base_file_name_from_template_name(template_file_name) do
+    [base, extension, @eex_extension] = String.split(template_file_name,@file_separator)
+    base <> @file_separator <> extension
+  end
+
+  def fix_up(pn) do
     package_details = initialize_package(pn)
     original_dir = File.cwd()
     package_dir = package_details.template_dir
     File.cd package_dir
     {:ok,file_list} = File.ls()
-    for f <- file_list do
+    filtered_file_list = filter_file_list(file_list)
+    for f <- filtered_file_list do
       File.write(package_dir <> "/" <> get_base_file_name_from_template_name(f), EEx.eval_file(f))
     end
     File.cd original_dir
   end
 
-  def get_file_and_sha256(pn = @elixir_package_name) do
+  def get_file_and_sha256(pn) do
     package_details = initialize_package(pn)
     download_directory = package_details.download_directory
     filename = package_details.binary_name
@@ -89,50 +95,39 @@ defmodule ChocoUtil do
     get_sha256(download_directory <> filename)
   end
 
-  def get_file_and_sha256(pn = @rebar_package_name) do
-    package_details = initialize_package(pn)
-    download_directory = package_details.download_directory
-    filename = package_details.binary_name
-    remote_url = package_details.url_template
-    get_file_from_remote(download_directory, filename, remote_url)
-    get_sha256(download_directory <> filename)
-  end
+  # def get_file_and_sha256(pn = @rebar_package_name) do
+  #   package_details = initialize_package(pn)
+  #   download_directory = package_details.download_directory
+  #   filename = package_details.binary_name
+  #   remote_url = package_details.url_template
+  #   get_file_from_remote(download_directory, filename, remote_url)
+  #   get_sha256(download_directory <> filename)
+  # end
 
-  def get_file_and_sha256(pn = @erlang_x86_package_name) do
-    package_details_x86 = initialize_package(pn)
-    download_directory = package_details_x86.download_directory
-    filename = package_details_x86.binary_name
-    remote_url = package_details_x86.url_template
-    get_file_from_remote(download_directory, filename, remote_url)
-    get_sha256(download_directory <> filename)
-  end
+  # def get_file_and_sha256(pn = @erlang_x86_package_name) do
+  #   package_details_x86 = initialize_package(pn)
+  #   download_directory = package_details_x86.download_directory
+  #   filename = package_details_x86.binary_name
+  #   remote_url = package_details_x86.url_template
+  #   get_file_from_remote(download_directory, filename, remote_url)
+  #   get_sha256(download_directory <> filename)
+  # end
  
-  def get_file_and_sha256(pn = @erlang_x64_package_name) do
-    package_details_x64 = initialize_package(pn)
-    download_directory = package_details_x64.download_directory
-    filename = package_details_x64.binary_name
-    remote_url = package_details_x64.url_template
-    get_file_from_remote(download_directory, filename, remote_url)
-    get_sha256(download_directory <> filename)
-  end
+  # def get_file_and_sha256(pn = @erlang_x64_package_name) do
+  #   package_details_x64 = initialize_package(pn)
+  #   download_directory = package_details_x64.download_directory
+  #   filename = package_details_x64.binary_name
+  #   remote_url = package_details_x64.url_template
+  #   get_file_from_remote(download_directory, filename, remote_url)
+  #   get_sha256(download_directory <> filename)
+  # end
 
-  def get_current_version(pn = @elixir_package_name) do
+  def get_current_version(pn) do
     p = initialize_package(pn)
     p.current_version
   end
 
-  def get_current_version(pn = @erlang_x86_package_name) do
-    p = initialize_package(pn)
-    # Assume that x86 and x64 versions will always be the same
-    p.current_version
-  end
-
-  def get_current_version(pn = @rebar_package_name) do
-    p = initialize_package(pn)
-    p.current_version
-  end
-
-  def get_current_erts_version(pn = @erlang_x86_package_name) do
+  def get_current_erts_version(pn) do
     p = initialize_package(pn)
     p.current_erts_version
   end 
