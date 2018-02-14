b44516d0a06167f74446b3dd3114c44ea306d177
Onorio Catenacci <catenacci@ieee.org>
2018-02-13 19:26:37 -0500

    Reverting Erlang to 20.2
    I reverted Erlang to 20.2 since there is no prebuilt binary for 20.3
    (which is actually 20.2.3).  I also changed the URL's to use the
    https:// version by default.

--- lib/choco_util.ex
+++ lib/choco_util.ex
@@ -3,7 +3,7 @@ defmodule ChocoUtil do
   @elixir_ver "1.6.0"
   @erlang_x86_package_name "Erlang_x86"
   @erlang_x64_package_name "Erlang_x64"
-  @erlang_ver "20.3"
+  @erlang_ver "20.2"
   @rebar_package_name "Rebar3"
   @rebar_ver "3.5.0"
   @file_separator "."
@@ -35,9 +35,9 @@ defmodule ChocoUtil do
 
   defp initialize_package(@erlang_x86_package_name) do
     erlang_w32_package = %Package{
-      url_template: 'http://www.erlang.org/download/otp_win32_#{@erlang_ver}.exe',
+      url_template: 'https://www.erlang.org/download/otp_win32_#{@erlang_ver}.exe',
       current_version: "#{@erlang_ver}",
-      current_erts_version: "9.2.1",
+      current_erts_version: "9.2",
       binary_name: "/otp_win32_#{@erlang_ver}.exe",
       template_dir: @base_template_dir <> "erlang"
     }
@@ -47,9 +47,9 @@ defmodule ChocoUtil do
 
   defp initialize_package(@erlang_x64_package_name) do
     erlang_w64_package = %Package{
-      url_template: 'http://www.erlang.org/download/otp_win64_#{@erlang_ver}.exe',
+      url_template: 'https://www.erlang.org/download/otp_win64_#{@erlang_ver}.exe',
       current_version: "#{@erlang_ver}",
-      current_erts_version: "9.2.1",
+      current_erts_version: "9.2",
       binary_name: "/otp_win64_#{@erlang_ver}.exe",
       template_dir: @base_template_dir <> "erlang"
     }
